077e4bb9818eebc3498ba266bb3973986095ce87
bekzod <bekzodbaratov454@gmail.com>
2026-08-19 11:52:12 +0500

    ss

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -3,7 +3,7 @@ import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
 
 export const baseAPI = axios.create({
-  baseURL: `${API_URL}`,
+  baseURL: `${API_URL}/api/v1`,
   headers: {
     "Content-Type": "application/json",
   },
